7916af70e26171703dc8d05bddd776be6b4ab5d4
Daisho <daishokomiyama@gmail.com>
2018-04-15 18:24:00 -0400

    Added function to close modal

--- task4/js/header.js
+++ task4/js/header.js
@@ -1,23 +1,42 @@
 var Header = (function() {
 
-	var modal;
+	var modal, content, close
+	;
 
 	function clickHeaderLink(event) {
+		if ($modal.is(':visible')) {
+			closeModal(event);
+			return false;
+		}
+
 		event.preventDefault();
 		event.stopPropagation();
 		event.stopImmediatePropagation();
 
-		var url = $(event.target).attr("href");
+		var url = $(event.target).attr('href');
 
-		$.ajax(url,{ dataType: "text" })
+		$.ajax(url,{ dataType: 'text' })
 		.then(function(contents){
-			$modal.html(contents).show();
+			$content.html(contents);
+			$modal.show();
 		});
 	}
 
+	function closeModal(event) {
+		event.preventDefault();
+
+		$content.empty();
+		$modal.hide();
+	}
+
 	function init() {
-		$modal = $("[rel=js-modal]");
-		$("[rel=js-header]").on("click","> [rel^=js]", clickHeaderLink);
+		$modal = $('[rel="js-modal"]');
+		$close = $modal.children('[rel="js-close"]');
+		$content = $modal.children('[rel="js-content"]');
+
+		$('[rel=js-header]').on('click','> [rel^=js]', clickHeaderLink);
+
+		$close.on('click', closeModal);
 	}
 
 	return {
